b2c452d0d2c44c595c90bfe1fb95bcbee9897224
OmarMesqq <omarmsqt@gmail.com>
2025-04-21 18:13:48 -0300

    refactor: :recycle: add RFC on access functions for Team entity

--- time.h
+++ time.h
@@ -1,5 +1,37 @@
 #ifndef TIME_H
 #define TIME_H
+/* 
+* Esse módulo (Time) atua como servidor dos módulos Principal e Gerenciamento de Tarefas (GT) 
+*/
+
+typedef struct time {
+    char* nome;
+} Time;
+
+
+/*
+ * Retorna ponteiro válido para time caso criação seja bem sucedida
+ * Retorna NULL em caso de falha
+ * */
+Time* cria_time(char* nome);
+
+/*
+ * Retorna ponteiro válido para time caso consulta seja bem sucedida
+ * Retorna NULL em caso de falha
+ * */
+Time* consulta_time(char* nome);
+
+/*
+ * Retorna ponteiro válido para time caso alteração seja bem sucedida
+ * Retorna NULL em caso de falha
+ * */
+Time* altera_time(char* nome, Time* timePatch);
+
+/*
+ * Retorna 1 caso a remoção seja bem sucedida 
+ * Retorna 0 em caso contrário
+ * */
+int apaga_time(char* nome);
 
 
 #endif
